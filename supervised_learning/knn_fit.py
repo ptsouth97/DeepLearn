dbc12c3e930622f054eba9feb0638b89ecf3de6d
Blake Crosby <ptsouth97@gmail.com>
2018-07-20 09:48:45 -0400

    changed y/n/? to 1/0/nan in df and dropped na

--- supervised_learning/knn_fit.py
+++ supervised_learning/knn_fit.py
@@ -1,3 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.5
 
-print('Hello world!')
+from sklearn.neighbors import KNeighborsClassifier
+import load_data
+
+
+def main():
+	''' main function'''
+
+	df = load_data.main()
+
+	# Create arrays for the features and the response variable
+	y = df['party'].values
+	X = df.drop('party', axis=1).values
+
+	# Create a k-NN classifier with 6 neighbors
+	knn = KNeighborsClassifier(n_neighbors=6)
+
+	# Fit the classifier to the data
+	knn.fit(X, y)
+
+
+if __name__ == '__main__':
+	main()
--- supervised_learning/load_data.py
+++ supervised_learning/load_data.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 
 import pandas as pd
+import numpy as np
+
 
 def main():
 	'''load file votes.csv'''
 	
 	data_file = 'votes.csv'
 	df = pd.read_csv(data_file)
+	df.replace(('y', 'n'), (1, 0), inplace=True)
+	df[df == '?'] = np.nan
+	df = df.dropna()
+
 	print(df)
+
+	return df
 
 
 if __name__ == '__main__':
