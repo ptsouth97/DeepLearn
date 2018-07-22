877ffc13b48d001e671fa924cc9b0d25649abdb8
Blake Crosby <ptsouth97@gmail.com>
2018-07-21 23:58:51 -0400

    minor updates

--- supervised_learning/knn_fit.py
+++ supervised_learning/knn_fit.py
@@ -24,12 +24,13 @@ def main():
 	# Predict the labels for the training data X
 	y_pred = knn.predict(X)
 
-	# Predict and print the label for the new data point X_new
+	# Make a new data point X_new to test
 	X_new = pd.DataFrame([0.041569, 0.717944, 0.108728, 0.417277, \
   						  0.414185, 0.397297, 0.117327, 0.306765, \
 						  0.988804, 0.358076, 0.667909, 0.019063, \
 						  0.079745, 0.809151, 0.343293, 0.889644]).T   
 
+	# Predict and print the label for the new data point X_new
 	new_prediction = knn.predict(X_new)
 	print("Prediction: {}".format(new_prediction))
 
