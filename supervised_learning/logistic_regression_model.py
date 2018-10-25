fb5f126e11e180444e892b479e23d58e2d2d8b69
ptsouth97 <ptsouth97@gmail.com>
2018-10-24 21:30:45 -0400

    rearranged folders

--- supervised_learning/logistic_regression_model.py
+++ supervised_learning/logistic_regression_model.py
@@ -5,35 +5,32 @@ from sklearn.metrics import confusion_matrix
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-
-
+from sklearn.linear_model import LogisticRegression
+ 
+ 
 def main():
 	'''Evaluates the performance of a binary classifier by computing a confusion matrix and generating a classification report'''
-
+ 
 	file_name = 'diabetes.csv'
 	df = pd.read_csv(file_name)
-
+ 
 	y = np.array(df['Outcome'])
 	X = np.array(df.drop('Outcome', axis=1))
 
 	# Create training and test set
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
 
-	# Instantiate a k-NN classifier: knn
-	knn = KNeighborsClassifier(n_neighbors=6)
+	# Create the classifier: logreg
+	logreg = LogisticRegression()
 
 	# Fit the classifier to the training data
-	knn.fit(X_train, y_train)
+	logreg.fit(X_train, y_train)
 
-	# Predict the labels of the test data: y_pred
-	y_pred = knn.predict(X_test)
+	# Predict the labels of the test set: y_pred
+	y_pred = logreg.predict(X_test)
 
-	# Generate the confusion matrix and classification report
-	print('CONFUSION MATRIX')
+	# Compute and print the confusion matrix and classification report
 	print(confusion_matrix(y_test, y_pred))
-	print('')
-	print('CLASSIFICATION REPORT')
 	print(classification_report(y_test, y_pred))
 
 
